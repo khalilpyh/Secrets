6892b82331ce10c4184d80278148ebc64de337e6
khalilpyh <khalilpyh@gmail.com>
2023-07-13 10:41:32 -0400

    change authentication method of using md5/bcrypt

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt  = require("mongoose-encryption");
-
+// const encrypt = require("mongoose-encryption");
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -26,8 +28,11 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-//password encryption
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+// //password encryption
+// userSchema.plugin(encrypt, {
+//   secret: process.env.SECRET,
+//   encryptedFields: ["password"],
+// });
 
 const User = mongoose.model("User", userSchema);
 
@@ -42,6 +47,7 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
+  // const password = md5(req.body.password);  //hash password
   const password = req.body.password;
 
   //find user in database by username(email)
@@ -50,16 +56,18 @@ app.post("/login", (req, res) => {
       //if found a matching user
       if (foundUser) {
         //check password
-        if (foundUser.password === password) {
-          res.render("secrets");
-        } else {
-          //display error message to user
-          res.render("login", {
-            username: username,
-            password: password,
-            errorMessage: "Incorrect username or password.",
-          });
-        }
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (result === true) {
+            res.render("secrets");
+          } else {
+            //display error message to user
+            res.render("login", {
+              username: username,
+              password: password,
+              errorMessage: "Incorrect username or password.",
+            });
+          }
+        });
       } else {
         //if no user matches
         res.render("login", {
@@ -77,22 +85,26 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  //create new user obj base on user input value
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password,
-  });
+  bcrypt.hash(req.body.password, saltRounds, function (error, hash) {
+    //create new user obj base on user input value
+    const newUser = new User({
+      email: req.body.username,
+      // password: md5(req.body.password),  //hash password
+      password: hash,
+    });
 
-  //save new user to database
-  newUser
-    .save()
-    .then(() => {
-      //navigate to secret page
-      res.render("secrets");
-    })
-    .catch((err) => console.log(err));
+    //save new user to database
+    newUser
+      .save()
+      .then(() => {
+        //navigate to secret page
+        res.render("secrets");
+      })
+      .catch((err) => console.log(err));
+  });
 });
 
+//set up port
 app.listen(3000, () => {
   console.log("Server started on port 3000.");
 });
