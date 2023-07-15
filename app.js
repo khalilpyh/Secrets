402b0d7dba215627cfa6739f275061b3acee269c
khalilpyh <khalilpyh@gmail.com>
2023-07-15 09:40:19 -0400

    add in functiion for user to submit secrets

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
   googleId: String, //for google auth
+  secret: String, //for holding user secrets
 });
 
 //plugin passport local mongooseF
@@ -51,11 +52,16 @@ const User = mongoose.model("User", userSchema);
 
 //passport configuration
 passport.use(User.createStrategy());
-passport.serializeUser(function (user, done) {
-  done(null, user);
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    cb(null, { id: user.id, username: user.username, name: user.name });
+  });
 });
-passport.deserializeUser(function (user, done) {
-  done(null, user);
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
 });
 
 //google auth configuration
@@ -161,10 +167,54 @@ app.post("/register", (req, res) => {
 app.get("/secrets", (req, res) => {
   //check if user is authenticated
   if (req.isAuthenticated()) {
-    res.render("secrets");
+    User.find({ secret: { $ne: null } })
+      .then((foundUsers) => {
+        if (foundUsers) {
+          //navigate to secret page with all secrets
+          res.render("secrets", { usersWithSecrets: foundUsers });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   } else {
     res.redirect("/login");
   }
+});
+
+//Get - Submit Page
+app.get("/submit", (req, res) => {
+  //check if user is authenticated
+  if (req.isAuthenticated()) {
+    res.render("submit");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+//Post - Submit Page
+app.post("/submit", (req, res) => {
+  const submittedSecret = req.body.secret;
+
+  //find the user who submitted secret
+  User.findById(req.user.id)
+    .then((foundUser) => {
+      if (foundUser) {
+        //save the submitted secret and redirect to secret page
+        foundUser.secret = submittedSecret;
+        foundUser
+          .save()
+          .then(() => {
+            res.redirect("/secrets");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 //set up port
